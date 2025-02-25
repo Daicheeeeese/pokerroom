3717e479c73b3d2922808b7c9d68b7cbe57ddf40
Daicheeeeese <daicheese7@gmail.com>
2025-02-26 00:11:37 +0900

    fix: remove unused useCallback import

--- src/components/rooms/LocationMap.tsx
+++ src/components/rooms/LocationMap.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useCallback, useMemo } from 'react'
+import { useMemo } from 'react'
 import { GoogleMap, useLoadScript, Libraries, Marker } from '@react-google-maps/api'
 
 type LocationMapProps = {
