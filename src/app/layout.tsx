bd79be24fd55b5a8796eaa06f99425737a28f213
Daicheeeeese <daicheese7@gmail.com>
2025-03-09 23:00:51 +0900

    remove: test-upload page and related components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,6 +41,7 @@ export default function RootLayout({
           </NextAuthProvider>
           <Toaster />
         </div>
+        <script src="https://widget.cloudinary.com/v2.0/global/all.js" type="text/javascript"></script>
       </body>
     </html>
   );
