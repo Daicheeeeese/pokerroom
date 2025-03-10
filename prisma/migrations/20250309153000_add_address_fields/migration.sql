480c6477e72ab330a0d6b05c27edf2bbea7d95e9
Daicheeeeese <daicheese7@gmail.com>
2025-03-10 23:13:20 +0900

    fix: recreate migrations

--- prisma/migrations/20250309153000_add_address_fields/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "rooms" ADD COLUMN "address" TEXT,
-                    ADD COLUMN "prefecture" TEXT,
-                    ADD COLUMN "city" TEXT,
-                    ADD COLUMN "latitude" DOUBLE PRECISION,
-                    ADD COLUMN "longitude" DOUBLE PRECISION; 
