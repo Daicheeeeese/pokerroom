56f5cd155111ece60ba64eff968fcf755e88a6b2
Daicheeeeese <daicheese7@gmail.com>
2025-03-11 00:59:52 +0900

    feat: add global prisma client for hot reload support

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -66,10 +66,8 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma
-}
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 export default prisma 
