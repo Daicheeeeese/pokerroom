15acc5c72ef81581884f94d64552e89c442d7499
Daicheeeeese <daicheese7@gmail.com>
2025-04-02 16:37:19 +0900

    fix: マイグレーションファイルを再生成

--- prisma/migrations/20250402073704_init/migration.sql
+++ prisma/migrations/20250402073704_init/migration.sql
@@ -96,6 +96,7 @@ CREATE TABLE "reservations" (
     "status" "ReservationStatus" NOT NULL DEFAULT 'PENDING',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "request" TEXT,
 
     CONSTRAINT "reservations_pkey" PRIMARY KEY ("id")
 );
