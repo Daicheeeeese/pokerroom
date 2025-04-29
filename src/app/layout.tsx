8c69dae7e4da91c22b1c48fda7816cd384e7143a
Daicheeeeese <daicheese7@gmail.com>
2025-04-30 01:31:40 +0900

    feat: Hotjarのトラッキングコードを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,6 +44,18 @@ export default function RootLayout({
             gtag('config', 'G-DR0ZCLKC4T');
           `}
         </Script>
+        <Script id="hotjar" strategy="afterInteractive">
+          {`
+            (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:6387251,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `}
+        </Script>
       </head>
       <body className={`${inter.className} antialiased`} suppressHydrationWarning>
         <div className="flex flex-col min-h-screen">
